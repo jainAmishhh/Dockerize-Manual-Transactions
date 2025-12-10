1cae915557a05fbc39bafef5cdec813836a21a5f
Amish Jain <jainamish1511@gmail.com>
2025-12-10 13:09:30 +0530

    clean App.jsx and integrate TransactionHistory page

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TransactionHistory from './pages/transactionHistory/TransactionHistory';
+  
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <TransactionHistory />
+    </div>
+  )
+}
+
+export default App;
